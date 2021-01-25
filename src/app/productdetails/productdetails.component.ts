fbbed24e98603a096540ba197102c9356f3435fc
mukeshmjn <68895120+mukeshmjn@users.noreply.github.com>
2021-01-25 16:58:05 +0530

    queryParams

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductdetailService } from '../services/productdetail.service'
-
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {HomeService} from '../services/home.service';
 @Component({
   selector: 'app-productdetails',
   templateUrl: './productdetails.component.html',
@@ -8,16 +9,25 @@ import { ProductdetailService } from '../services/productdetail.service'
 })
 export class ProductdetailsComponent implements OnInit {
 
-  constructor(private pdetails: ProductdetailService) { }
+  constructor(private hs: HomeService,private pdetails: ProductdetailService,private activatedRoute: ActivatedRoute) { }
   product: any={};
+  queryParams:any
   ngOnInit() {
-
-    this.pdetails.pdetails.subscribe(mesg =>{
+    this.activatedRoute.queryParams.subscribe(params => {
+      const docId = params['search'];
+      console.log('docId: ',docId);
+      this.hs.getProduct(docId).subscribe(data=>{
+        //console.log('book data: ',data.data())
+        this.product = data.data(); 
+      })
+    });
+//console.log('params: ',this.queryParams)
+    // this.pdetails.pdetails.subscribe(mesg =>{//behaviour subject method removed 
      
-      console.log(mesg);
-      this.product = mesg.value;
-      console.log(this.product)
-    })
+    //   console.log(mesg);
+    //   this.product = mesg.value;
+    //   console.log(this.product)
+    // })
 
   }
 
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Home } from 'src/app/home.model';
+import { Observable } from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
@@ -18,6 +19,11 @@ createBook(home: Home){
 
 deleteBook(homeId: string){
   this.firestore.doc('home/' + homeId).delete();
+}
+
+getProduct(id: string){
+  
+  return this.firestore.collection('home').doc(id).get()
 }
 
 
